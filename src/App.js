572ba30961776d802369e6f68b0f520166f194b9
PxS00 <lucas.rossonidd@gmail.com>
2025-07-10 16:43:12 -0300

    refactor: Replace logo implementation with a new component and remove unused CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from "./img/logo.svg";
+import Logo from "./components/logo";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="logo">
-          <img src={logo} alt="logo"></img>
-          <p><strong>Alura</strong> Books</p>
-        </div>
+        <Logo></Logo>
       </header>
     </div>
   );
