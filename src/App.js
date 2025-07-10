e7c46f45c4d430e983f29c9768017ec0628d0617
PxS00 <lucas.rossonidd@gmail.com>
2025-07-10 17:24:24 -0300

    feat: Enhance App component layout with new options and icon displays

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import Logo from './components/logo';
 import './App.css';
+import perfil from './img/perfil.svg';
+import sacola from './img/sacola.svg';
+
+
+const textOptions = ['Categorias', 'Minha Estante', 'Favoritos'];
+const icons = [sacola, perfil];
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
+        <ul className='options'>
+          {textOptions.map((text) => (
+            <li className = 'option'><p>{text}</p></li>
+          ))}
+        </ul>
+
+        <ul className='icons'>
+          {icons.map((icon) => (
+            <li className='icon'><img src={icon} alt='icon'></img></li>
+          ))}
+        </ul>
       </header>
     </div>
   );
